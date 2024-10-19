0b3ac42866d3222ef42afe6cf790686d7f5d0836
jgsheffer <jgsheffe@gmail.com>
2024-10-19 18:20:42 -0400

    Fixing dumb typo

--- __tests__/part1.spec.ts
+++ __tests__/part1.spec.ts
@@ -2,20 +2,20 @@
 import  {calculatePosition, Command }  from '../src/submarine';
 
 describe('Submarine Position Calculator', () => {
-  it('returns {horizontal: 0, depth: 0} for an empty input', () => {
-    expect(calculatePosition([])).toEqual({ horizontal: 0, depth: 0 });
+  it('returns {horizontal: 0, depth: 0, product:0} for an empty input', () => {
+    expect(calculatePosition([])).toEqual({ horizontal: 0, depth: 0, product: 0 });
   });
 
   it('correctly calculates position for a single forward command', () => {
-    expect(calculatePosition(['forward 5'])).toEqual({ horizontal: 5, depth: 0 });
+    expect(calculatePosition(['forward 5'])).toEqual({ horizontal: 5, depth: 0, product: 0 });
   });
 
   it('correctly calculates position for a single down command', () => {
-    expect(calculatePosition(['down 3'])).toEqual({ horizontal: 0, depth: 3 });
+    expect(calculatePosition(['down 3'])).toEqual({ horizontal: 0, depth: 3,  product: 0 });
   });
 
   it('correctly calculates position for a single up command', () => {
-    expect(calculatePosition(['up 2'])).toEqual({ horizontal: 0, depth: 0 });
+    expect(calculatePosition(['up 2'])).toEqual({ horizontal: 0, depth: 0, product: 0 });
   });
 
   it('correctly calculates position for multiple commands', () => {
@@ -27,7 +27,7 @@ describe('Submarine Position Calculator', () => {
       'down 8',
       'forward 2'
     ];
-    expect(calculatePosition(commands)).toEqual({ horizontal: 15, depth: 10 });
+    expect(calculatePosition(commands)).toEqual({ horizontal: 15, depth: 10, product: 150 });
   });
 
   it('handles negative depths (submarines cant fly)', () => {
@@ -36,7 +36,7 @@ describe('Submarine Position Calculator', () => {
       'forward 3',
       'down 2'
     ];
-    expect(calculatePosition(commands)).toEqual({ horizontal: 3, depth: 0 });
+    expect(calculatePosition(commands)).toEqual({ horizontal: 3, depth: 0, product: 0 });
   });
 
   it('handles large numbers', () => {
@@ -45,7 +45,7 @@ describe('Submarine Position Calculator', () => {
       'down 2000000',
       'up 1000000'
     ];
-    expect(calculatePosition(commands)).toEqual({ horizontal: 1000000, depth: 1000000 });
+    expect(calculatePosition(commands)).toEqual({ horizontal: 1000000, depth: 1000000, product: 1000000000000 });
   });
 
   it('throws an error for invalid commands', () => {
