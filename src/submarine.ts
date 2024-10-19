9e86f84051c19f52464134a9fdd311731f167898
jgsheffer <jgsheffe@gmail.com>
2024-10-19 18:03:14 -0400

    Adding types to the calculate

--- src/submarine.ts
+++ src/submarine.ts
@@ -1,3 +1,12 @@
-export const calculatePosition = (commands: any[]): any => {
+
+export type Command = `${'forward' | 'up' | 'down'} ${number}`;
+
+export interface Position {
+  horizontal: number;
+  depth: number;
+}
+
+export const calculatePosition = (commands: Command[]): Position => {
+  return { horizontal: 0, depth: 0 };
 }
 
